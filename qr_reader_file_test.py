efbb5dd0b983921a0388cecb871e8d1160413c6c
Skel-Enkai <williamwarrenmeeks@gmail.com>
2025-05-29 20:59:11 +0100

    qr_reader test for realtime (no file access)

--- qr_reader_file_test.py
+++ qr_reader_file_test.py
@@ -8,15 +8,14 @@ import cv2
 cam = Picamera2()
 capture_config = cam.create_still_configuration()
 cam.start()
+cam.switch_mode(capture_config)    
 
 qreader = QReader()
-
-cam.switch_mode(capture_config)    
 
 while True:
     time.sleep(1)
     
-    cam.capture_file("temp.png")
+    cam.capture_file( "temp.png")    
     
     image = cv2.cvtColor(cv2.imread("temp.png"), cv2.COLOR_BGR2RGB)
 
--- qr_reader_test.py
+++ qr_reader_test.py
@@ -1,26 +1,23 @@
 import time
 from picamera2 import Picamera2
-import io
 
 from qreader import QReader
-import numpy as np
-
 import cv2
+
+qreader = QReader()
 
 cam = Picamera2()
 cam.start()
 
-qreader = QReader()
+capture_config = cam.create_still_configuration()
+cam.switch_mode(capture_config)    
 
 while True:
     time.sleep(1)
-    data = io.BytesIO()
-    cam.capture_file(data, format='png')
     
-    file_bytes = np.asarray(bytearray(data.read()), dtype=np.uint8)
-    
-    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    img = cam.capture_array()
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    qreader_out = qreader.detect_and_decode(image=img)
+    qreader_out = qreader.detect_and_decode(image=image)
     print(qreader_out)
 
