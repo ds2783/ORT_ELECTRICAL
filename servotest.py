1d374f80d06a41dc6af5eaf050501fd21f51a830
ds2783 <ds2783@bath.ac.uk>
2025-05-30 14:44:31 +0100

    update

--- servotest.py
+++ servotest.py
@@ -10,8 +10,10 @@ from adafruit_servokit import ServoKit
 # Set channels to the number of servo channels on your kit.
 # 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
 kit = ServoKit(channels=16)
-
-while True:
+now = time.time()
+start = time.time()
+while now<(start + 10):
+    now = time.time()
     for i in range(0,16):
         kit.continuous_servo[i].throttle = 1
     time.sleep(2)
