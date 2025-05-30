edf3c457bf30a3ce68ff488db0c7c0546cca0c3b
ds2783 <ds2783@bath.ac.uk>
2025-05-30 14:41:37 +0100

    updated

--- servotest.py
+++ servotest.py
@@ -1,1 +1,22 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+"""Simple test for a standard servo on channel 0 and a continuous rotation servo on channel 1."""
+
 import time
+
+from adafruit_servokit import ServoKit
+
+# Set channels to the number of servo channels on your kit.
+# 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
+kit = ServoKit(channels=16)
+
+while True:
+    for i in range(0,16):
+        kit.continuous_servo[i].throttle = 1
+    time.sleep(2)
+    for i in range (0,16):
+        kit.continuous_servo[i].throttle = -1
+    time.sleep(2)
+    for i in range (0,16):
+        kit.continuous_servo[i].throttle = 0
