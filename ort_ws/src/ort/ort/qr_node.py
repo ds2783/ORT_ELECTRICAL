564393df3cd3b4bde0ac178462243492bfb10ad3
Skel-Enkai <williamwarrenmeeks@gmail.com>
2025-05-30 14:54:14 +0100

    ORT-WS, with outlines of Teleop added.

---
+++ ort_ws/src/ort/ort/qr_node.py
@@ -0,0 +1,16 @@
+# IN PROGRESS
+
+import rclpy
+from rclpy.node import Node
+
+import cv2
+from qreader import QReader
+from picamera2 import Picamera2
+
+class QRCam(Node):
+    def __init__(self):
+        super().__init__('qr_node')
+
+def main():
+    print('Hi from ort.')
+
---
+++ ort_ws/src/ort/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'ort'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools', 'qreader', 'picamera2'],
+    zip_safe=True,
+    maintainer='william',
+    maintainer_email='williamwarrenmeeks@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'qr_node = ort.qr_node:main',
+            'teleop = ort.teleop:main'
+        ],
+    },
+)
