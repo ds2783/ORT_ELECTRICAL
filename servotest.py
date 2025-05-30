162767899c07e039a0d7f69874d58844b4d08a9b
ds2783 <ds2783@bath.ac.uk>
2025-05-30 17:01:04 +0100

    update

--- servotest.py
+++ servotest.py
@@ -1,10 +1,4 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-"""Simple test for a standard servo on channel 0 and a continuous rotation servo on channel 1."""
-
 import time
-
 from adafruit_servokit import ServoKit
 
 # Set channels to the number of servo channels on your kit.
