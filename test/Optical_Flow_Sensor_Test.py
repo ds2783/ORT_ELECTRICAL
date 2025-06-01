a863adb4102f13cd290681c467027f687b3b8215
ds2783 <ds2783@bath.ac.uk>
2025-06-01 11:11:21 +0100

    Update Optical_Flow_Sensor_Test.py

--- test/Optical_Flow_Sensor_Test.py
+++ test/Optical_Flow_Sensor_Test.py
@@ -1,7 +1,7 @@
-  from pmw3901 import PMW3901
-  sensor = PMW3901(spi_bus=0, cs_pin=7)
-  sensor.begin()
-  while True:
-      if sensor.motion_ready():
-          x, y = sensor.read_motion()
-          print(f"X: {x}, Y: {y}")
+from pmw3901 import PMW3901
+sensor = PMW3901(spi_bus=0, cs_pin=7)
+sensor.begin()
+while True:
+    if sensor.motion_ready():
+        x, y = sensor.read_motion()
+        print(f"X: {x}, Y: {y}")
