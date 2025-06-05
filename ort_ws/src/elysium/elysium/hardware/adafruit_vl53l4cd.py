aad706533e41befe985142682178cb2734bef0fe
Y.Nijo <twister2341@outlook.com>
2025-06-05 16:06:08 +0100

    LOGGING CODE 3

--- ort_ws/src/elysium/elysium/hardware/adafruit_vl53l4cd.py
+++ ort_ws/src/elysium/elysium/hardware/adafruit_vl53l4cd.py
@@ -498,12 +498,14 @@ class VL53L4CD:
     def _read_register(self, register, length=1):
         
         data = bytearray(length)
+        print(f"DATA WRITE TO REG: {register}")
         self.i2c_bus.write_byte(self.i2c_address, int.from_bytes(struct.pack(">H", register)))
+
         for b in range(length):
             data[b] = self.i2c_bus.read_byte(self.i2c_address)
 
         print(f"DATA READ: {data}")
-        
+
         return data
 
     def set_address(self, new_address):
