3dec3879fbe7be739e6be75feca66cee3f641589
Skel-Enkai <williamwarrenmeeks@gmail.com>
2025-06-10 18:12:23 +0100

    Fixes order

--- ort_ws/src/elysium/elysium/teleop.py
+++ ort_ws/src/elysium/elysium/teleop.py
@@ -85,7 +85,7 @@ class TelepresenceOperations(Node):
         # goes from 1 to -1, therefore difference between the two
         # should be halved.
         self.target.linear /= 2
-        self.target.rotation = msg.axes[AXES["LEFTX"]]
+        self.target.rotation = -msg.axes[AXES["LEFTX"]]
         # ------------------------
 
         self.z_increment = msg.axes[AXES["RIGHTX"]] * CAMERA_SENSITIVITY
@@ -133,7 +133,7 @@ class TelepresenceOperations(Node):
         # )
 
     def camera_rotate(self):
-        self.cam_angles_.z_axis = self.bound_180(float(self.z_increment + self.cam_angles_.z_axis))
+        self.cam_angles_.z_axis = self.bound_180(float(-self.z_increment + self.cam_angles_.z_axis))
         self.cam_angles_.x_axis = self.bound_180(float(self.x_increment + self.cam_angles_.x_axis))
         # POSITIONAL
         self.kit_.servo[CAMERA_SERVO_Z].angle = self.cam_angles_.z_axis
