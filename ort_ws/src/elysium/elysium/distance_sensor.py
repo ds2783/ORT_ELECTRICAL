ae16ec4f73961fb24468254fa4a1a3033a35a8c8
Y.Nijo <twister2341@outlook.com>
2025-06-10 12:32:27 +0100

    Time.sleep may be needed

--- ort_ws/src/elysium/elysium/distance_sensor.py
+++ ort_ws/src/elysium/elysium/distance_sensor.py
@@ -89,8 +89,11 @@ def main(args=None):
     xshut_pin = gpio.DigitalOutputDevice(17, pin_factory=factory)
     xshut_pin.off()
 
+    import time
     _distance_sensor_1 = DistanceNode(node_name_1, topic_name_1, i2c_addr=0x29, sleep_node=sleep_node)  
+    time.sleep(0.1)
     _distance_sensor_1.sensor.set_address(0x2A)
+    time.sleep(0.1)
     xshut_pin.on()
 
     _distance_sensor_2 = DistanceNode(node_name_2, topic_name_2, i2c_addr=0x29, sleep_node=sleep_node)
