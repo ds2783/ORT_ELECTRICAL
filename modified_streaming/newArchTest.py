24a0760e0f0dc93eebf29a14c337b74996ae6af0
Skel-Enkai <williamwarrenmeeks@gmail.com>
2025-06-20 17:39:12 +0100

    New ArchTest.py, stop complaining.

---
+++ modified_streaming/newArchTest.py
@@ -0,0 +1,49 @@
+import sys
+
+from StreamServer import StreamServer
+from Comms.Output import Output
+
+from signal import signal, SIGINT
+
+BASE_IP = '192.168.0.103'
+PI_IP = '192.168.0.101'
+# config
+MULTICAST=False
+PORT_MAIN_BASE=5008
+PORT_MAIN=5020
+
+MODEL="imx708_noir" # camera model name (find using libcamera-vid --list-cameras)
+WIDTH=1296
+HEIGHT=972
+NAME="camera" # stream name for display in console
+
+MODEL="imx219"
+IP_MAIN = IP_SECONDARY = BASE_IP
+
+out=Output("None") # console output, with optional TCP forwarding
+
+# Initialise stream
+stream=StreamServer(out,MODEL,NAME) # system finds the camera based upon the model number (assumes no duplicates)
+stream.configure(WIDTH,HEIGHT)
+stream.start_stream(IP_MAIN, PORT_MAIN) # using a multicast address 224.1.1.1:5008
+stream.start_server(PI_IP, PORT_MAIN_BASE)
+stream.set_bitrate(5000000)
+
+#stream.set_controls({"AfMode": controls.AfModeEnum.Continuous}) # set a libcamera control
+
+# exit handler
+def handler(signal_received,frame):
+    global run
+    run = False
+
+    stream.stop()
+    sys.exit()
+
+signal(SIGINT, handler)
+
+run = True
+while run:
+    try:
+        stream.run()
+    except:
+        pass
