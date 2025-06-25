d91e8fb382da33ab94c277141b3ad11b32f65ac8
Y.Nijo <twister2341@outlook.com>
2025-06-25 18:42:15 +0100

    Minor edits 3?

--- ort_ws/src/elysium/elysium/battery_monitor.py
+++ ort_ws/src/elysium/elysium/battery_monitor.py
@@ -167,7 +167,6 @@ class BatteryMonitorNode(Node):
         if Path(path).is_file():
             try:
                 dataframe = pandas.read_csv(path, sep=",", index_col=0, dtype=pandas.Float64Dtype())
-                self.get_logger().info(f"""DATAFRAME READ: {dataframe}""")
             except pandas.errors.EmptyDataError:
                 pass
 
