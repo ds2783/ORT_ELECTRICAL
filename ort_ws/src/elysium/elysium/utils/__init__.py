3f6388dc3f0fa45ca3d2e3e681ab0d5bb372c182
Y.Nijo <twister2341@outlook.com>
2025-06-29 16:25:18 +0100

    Added other integration code plus renamed for clarity

--- ort_ws/src/elysium/elysium/utils/__init__.py
+++ ort_ws/src/elysium/elysium/utils/__init__.py
@@ -4,10 +4,20 @@ class ApproxIntegration:
     def __init__(self, previous_y):
         self.prev_y = previous_y
 
-    def integ_single(self, y, dt):  # Trapezoidal rule approximate integration
+    def single(self, y, dt):  # Trapezoidal rule approximate integration
         ret = ((self.prev_y + y) / 2) * dt
         self.prev_y = y
         return ret
+
+    def quad_lite(self, func, N, upper_lim, lower_lim):
+        h = (upper_lim - lower_lim)/N
+        summation = 0
+        for i in range(1, N):
+            summation += func(lower_lim + i * h)
+        summation += (func(upper_lim) + func(lower_lim))/2
+        summation *= h
+        
+        return summation
 
 class RollingAverage:
     def __init__(self, length):
