0010ec5835e61307039b299788b0e1fd9afa2bda
Y.Nijo <twister2341@outlook.com>
2025-06-29 17:50:14 +0100

    Documentation

--- ort_ws/src/elysium/elysium/utils/__init__.py
+++ ort_ws/src/elysium/elysium/utils/__init__.py
@@ -33,7 +33,7 @@ class Integration:
 
 class RollingAverage:
     def __init__(self, dim):
-        """Rolling average for a ndarray or 2D ndarray.
+        """Rolling average for a ndarray or a 2D ndarray.
 
         :param dim: shape of numpy array, format of (Row, Column). The row will dictate how many samples to average over.
         :type dim: Iterable, Tuple, List
@@ -48,6 +48,13 @@ class RollingAverage:
         self._queue = np.zeros(dim, dtype=np.float32)
     
     def add(self, value):
+        """Add a value or set of values to the rolling average. If the shape of the 
+        rolling average is not (row, 1), make sure the row vector passed is the same shape as 
+        (1, col). (Basically make sure the row vector is the correct shape.)
+
+        :param value: Single value or row vector
+        :type value: int, np.ndarray
+        """
         if self.items < self.dim[0]:
             self.items += 1
     
